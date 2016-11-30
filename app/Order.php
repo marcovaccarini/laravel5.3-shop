c52a806a635bbfe4f0998843d1bf17ffe54a021e
marcovaccarini <quirkmode@yahoo.com>
2016-11-30 18:54:34 +0100

    add Order model, migration and pivot table

---
+++ app/Order.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    /**
+     * Only if the delivery address is different from the user address
+     *
+     * @var array
+     */
+    protected $fillable = array(
+        'user_id',
+        'delivery_first_name',
+        'delivery_last_name',
+        'delivery_address',
+        'delivery_address_2',
+        'delivery_city',
+        'delivery_state',
+        'delivery_zip',
+        'total',
+    );
+
+    /**
+     * An order can have many products
+     *
+     * @return $this
+     */
+    public function orderItems()
+    {
+        return $this->belongsToMany('App\Product')->withPivot('quantity', 'price', 'total');
+    }
+}
---
+++ database/migrations/2016_11_30_172156_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('delivery_first_name', 300);
+            $table->string('delivery_last_name', 300);
+            $table->string('delivery_address', 300);
+            $table->string('delivery_address_2', 300);
+            $table->string('delivery_city', 100);
+            $table->string('delivery_state', 100);
+            $table->string('delivery_zip', 10);
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
